4611635adcea13f80c7fb649d5fcb014dd006531
andywarthol <andywarthol@gmail.com>
2014-04-03 22:49:09 -0700

    Fixing carousel

--- wp-content/themes/builder/home.php
+++ wp-content/themes/builder/home.php
@@ -9,6 +9,7 @@ homepage of your website.
 */
 ?>
 <?php get_header(); ?>
+
 <section id="hero">
 	<div id="myCarousel" class="carousel slide">
 		<div class="carousel-inner">
@@ -22,28 +23,28 @@ homepage of your website.
 			<div class="item">
 				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga2.jpg" alt="">
 				<div class="carousel-caption">
-					<h4>Second Thumbnail label</h4>
+					<h4>First Thumbnail label</h4>
 					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				</div>
 			</div>
-			<div class="item active">
+			<div class="item">
 				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga3.jpg" alt="">
 				<div class="carousel-caption">
-					<h4>Third Thumbnail label</h4>
+					<h4>First Thumbnail label</h4>
 					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				</div>
 			</div>
-			<div class="item active">
+			<div class="item">
 				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga4.jpg" alt="">
 				<div class="carousel-caption">
-					<h4>Third Thumbnail label</h4>
+					<h4>First Thumbnail label</h4>
 					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				</div>
 			</div>
-			<div class="item active">
+			<div class="item">
 				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga5.jpg" alt="">
 				<div class="carousel-caption">
-					<h4>Third Thumbnail label</h4>
+					<h4>First Thumbnail label</h4>
 					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				</div>
 			</div>
@@ -52,10 +53,5 @@ homepage of your website.
 		<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
 	</div>
 </section>
-<script>
-	jQuery(document).ready(function($){
-		$('.carousel').carousel();
-	}); 
-</script>
 
 <?php get_footer(); ?>
