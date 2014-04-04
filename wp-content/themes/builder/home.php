88ab4ef1f0d1bad2de57b742baa75bab8509d570
XavierF <xavierf424@gmail.com>
2014-04-03 22:46:29 -0700

    xavier's imgs

--- wp-content/themes/builder/home.php
+++ wp-content/themes/builder/home.php
@@ -9,49 +9,53 @@ homepage of your website.
 */
 ?>
 <?php get_header(); ?>
-
-<div id="banner" class="home-page">
-	<div class="container">
-		<div id="inner-content" class="wrap clearfix">
-			<div class="jumbotron">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-7">
-							<h1>Hello, world!</h1>
-							<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-							<p><a class="btn btn-primary btn-lg">Learn more</a></p>
-						</div>
-						<div class="col-lg-5">
-							<p><img src="<?php echo get_template_directory_uri(); ?>/library/images/flat.png"></p>
-						</div>
-					</div><!-- end .row-->
-				</div> <!-- end .container-->
-			</div> <!-- end .jumbotron-->
-		</div> <!-- end #inner-content -->
-	</div> <!-- end .container-->
-</div> <!-- end #banner-->
-
-<div class="container home-three">
-	<div class="row">
-		<div class="col-lg-4">
-			<img class="img-circle" src="http://placehold.it/140x140">
-			<h2>Heading</h2>
-			<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-			<p><a class="btn btn-success" href="#">View details <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-		</div><!-- /.col-lg-4 -->
-		<div class="col-lg-4">
-			<img class="img-circle" src="http://placehold.it/140x140">
-			<h2>Heading</h2>
-			<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-			<p><a class="btn btn-success" href="#">View details <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-		</div><!-- /.col-lg-4 -->
-		<div class="col-lg-4">
-			<img class="img-circle" src="http://placehold.it/140x140">
-			<h2>Heading</h2>
-			<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-			<p><a class="btn btn-success" href="#">View details <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-		</div><!-- /.col-lg-4 -->
-	</div> <!-- end .row-->
-</div> <!-- end .container-->
+<section id="hero">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<div class="item">
+				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga1.jpg" alt="">
+				<div class="carousel-caption">
+					<h4>First Thumbnail label</h4>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga2.jpg" alt="">
+				<div class="carousel-caption">
+					<h4>Second Thumbnail label</h4>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+			</div>
+			<div class="item active">
+				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga3.jpg" alt="">
+				<div class="carousel-caption">
+					<h4>Third Thumbnail label</h4>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+			</div>
+			<div class="item active">
+				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga4.jpg" alt="">
+				<div class="carousel-caption">
+					<h4>Third Thumbnail label</h4>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+			</div>
+			<div class="item active">
+				<img src="<?php bloginfo('template_directory'); ?>/library/images/yoga5.jpg" alt="">
+				<div class="carousel-caption">
+					<h4>Third Thumbnail label</h4>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+	</div>
+</section>
+<script>
+	jQuery(document).ready(function($){
+		$('.carousel').carousel();
+	}); 
+</script>
 
 <?php get_footer(); ?>
