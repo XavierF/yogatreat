c57a3316799af0b551404418a8fe2dc8c684c0e4
XavierF <xavierf424@gmail.com>
2014-04-04 22:33:20 -0700

    Adding carousel items, fade

--- wp-content/themes/builder/home.php
+++ wp-content/themes/builder/home.php
@@ -11,22 +11,28 @@ homepage of your website.
 <?php get_header(); ?>
 
 <section id="hero" role="roll with it">
-	<div id="myCarousel" class="carousel slide">
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner">
-			<div class="item active">
-				<!-- <div class="carousel-caption">
-					<h4>First Thumbnail label</h4>
-					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				</div> -->
+			<div class="item active slide1">
+				<div class="curtain">&nbsp;</div>
+				<div class="carousel-caption">
+					<h1><?php bloginfo('description'); ?></h1>
+				</div>
 			</div>
-			<div class="item">
+			<div class="item slide2">
+				<div class="curtain">&nbsp;</div>
 				<div class="carousel-caption">
 					<h4>First Thumbnail label</h4>
 					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				</div>
 			</div>
-		<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-		<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+		 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
 	</div>
 </section>
 <section id="info">
@@ -36,7 +42,6 @@ Choose from a multitude of classes that guarantee a total mind, body and soul tr
 </p>
 </div>
 </section>
-
 <section id="services">
 	<div class="container">
 		<div class="row content">
@@ -46,24 +51,23 @@ Choose from a multitude of classes that guarantee a total mind, body and soul tr
 			<div class="col-md-4"><p>TRX</p></div>
 		</div><!-- .row -->
 		<div class="row content">
-	      <div class="col-md-4"><p>Tae Bo® Fitness</p></div>
-	      <div class="col-md-4"><p>Yoga Barre</p></div>  
-	      <div class="col-md-4"><p>Kids and Teens</p></div> 
-
+      <div class="col-md-4"><p>Tae Bo® Fitness</p></div>
+      <div class="col-md-4"><p>Yoga Barre</p></div>
+      <div class="col-md-4"><p>Kids and Teens</p></div>
 		</div><!-- .row -->
 	</div><!-- .container -->
-
-	
 </section>
-
 <section id="social">
 	<div class="container content">
 		<div class="row">
 			
 		</div><!-- .row -->
 	</div><!-- .container -->
-	
 </section>
 
-
+<script>
+	jQuery(document).ready(function($){
+		$('.carousel').carousel('pause');
+	});
+</script>
 <?php get_footer(); ?>
