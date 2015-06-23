6c0992e8098af67b65aa9e78d6f25dbf3b413a9f
XavierF <xavierf424@gmail.com>
2015-06-23 00:22:56 -0700

    updated css and promo btn

---
+++ footer.php
@@ -0,0 +1,36 @@
+			<footer class="footer" role="contentinfo">
+
+				<div class="container">
+
+				<div id="inner-footer" class="wrap clearfix">
+	<ul class="social">
+		<li><a href="https://www.facebook.com/pages/Yoga-Treat/1480466952175295" target="_blank"><i class="icon facebook"></i></a></li>
+		<li><a href="http://instagram.com/yogatreat?" target="_blank"><i class="icon instagram"></i></a></li>
+		<li><a href="http://twitter.com" target="_blank"><i class="icon twitter"></i></a></li>
+	</ul>
+
+					<nav role="navigation">
+							<?php bones_footer_links(); ?>
+					</nav>
+
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+
+				</div> <!-- end #inner-footer -->
+
+				</div> <!-- end .container -->
+
+			</footer> <!-- end footer -->
+
+		</div> <!-- end .wrapper -->
+
+		<!-- all js scripts are loaded in library/bones.php -->
+		<?php wp_footer(); ?>
+		<!-- CUSTOM JS  -->
+		<script>
+			
+				jQuery('li.trigger-contact a').attr('id', 'contact-us');
+			
+		</script>
+	</body>
+
+</html> <!-- end page. what a ride! -->
---
+++ home-page-template.php
@@ -0,0 +1,95 @@
+<?php 
+/**
+ * Home Page Template
+ *
+   Template Name:  Home Page Template
+ *
+ */
+?>
+<?php get_header(); ?>
+
+<section id="hero" role="roll with it">
+	<!--<p>
+		<a href="http://clients.mindbodyonline.com/ws.asp?studioid=150291&stype=-7&sTG=22&sVT=6&sView=week&sDate=6/2/2014" class="btn btn-primary btn-lg" target="_blank" >View class schedule &raquo;</a>
+	</p> -->
+	<!-- dynamic custom post url button code starts here -->
+	
+  <p>	
+	 <a href="<?php the_field('class_url_link'); ?>" class="btn btn-primary btn-lg" target="_blank" ><?php the_field('class_url_text'); ?></a>
+	</p>
+
+	 
+	<!-- and ends here -->
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1 class="tag"><?php $site_description = get_bloginfo( 'description', 'display' ); echo "$site_description";?></h1>
+				<h2>Choose from a multitude of classes that guarantee a total mind, body and soul transformation. <br class="visible-lg"/>Our classes will leave you feeling toned, energized and empowered.</h2>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="classes">
+	<div class="container">
+<!-- <a href="http://clients.mindbodyonline.com/ws.asp?studioid=150291&stype=-7&sView=week&sDate=5/10/2014" class="btn btn-primary btn-lg" target="_blank" >View available classes</a> -->
+		<div class="row content">
+			
+			<div class="col-md-4">
+				<a href="<?php the_field('class_1_url'); ?>" class="class-container">
+					<div class="class-thumb" style="background: url('<?php the_field('class_1_img');?>') center center no-repeat; background-size: cover;"></div>
+					<p class="class-text"><?php the_field('class_1_title'); ?></p>
+				</a> 
+			</div><!-- end .col-md-4 -->
+
+			<div class="col-md-4">
+				<a href="<?php the_field('class_2_url'); ?>" class="class-container">
+					<div class="class-thumb" style="background: url('<?php the_field('class_2_img');?>') center center no-repeat; background-size: cover;"></div>
+					<p class="class-text"><?php the_field('class_2_title'); ?></p>
+				</a> 
+			</div><!-- end .col-md-4 -->
+			
+			<div class="col-md-4">	
+				<a href="<?php the_field('class_3_url'); ?>" class="class-container">
+					<div class="class-thumb" style="background: url('<?php the_field('class_3_img');?>') center center no-repeat; background-size: cover;"></div>
+					<p class="class-text"><?php the_field('class_3_title'); ?></p>
+				</a> 
+			</div><!-- end .col-md-4 -->
+	
+      <div class="col-md-4">
+     		<a href="<?php the_field('class_4_url'); ?>" class="class-container">
+     			<div class="class-thumb" style="background: url('<?php the_field('class_4_img');?>') center center no-repeat; background-size: cover;"></div>
+      		<p class="class-text"><?php the_field('class_4_title'); ?></p>
+      	</a> 
+      </div>
+
+      <div class="col-md-4">
+      	<a href="<?php the_field('class_5_url'); ?>" class="class-container">
+      		<div class="class-thumb" style="background: url('<?php the_field('class_5_img');?>') center center no-repeat; background-size: cover;"></div>
+      		<p class="class-text"><?php the_field('class_5_title'); ?></p>
+      	</a>  
+      </div><!-- end .col-md-4 -->
+
+      <div class="col-md-4">
+				<a href="<?php the_field('class_6_url'); ?>" class="class-container">
+					<div class="class-thumb" style="background: url('<?php the_field('class_6_img');?>') center center no-repeat; background-size: cover;"></div>
+					<p class="class-text"><?php the_field('class_6_title'); ?></p>
+				</a> 
+      </div><!-- end .col-md-4 -->
+
+		</div><!-- .row -->
+	</div><!-- .container -->
+</section>
+<section id="social">
+	<div class="container content">
+		<div class="row">
+			
+		</div><!-- .row -->
+	</div><!-- .container -->
+</section>
+
+
+<script>
+//jQuery(document).ready(function($) { setTimeout(function() { $('#modal').modal('show'); }, 3000); });
+</script>
+<?php get_footer(); ?>
